9f662df2cacbe32f0484b4bd752cac1eac649dd2
Sonu Kumar Gautam <sonukumargautam41638@gmail.com>
2021-08-06 16:47:17 +0530

    final commit

--- patientapp/urls.py
+++ patientapp/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path
+from django.urls.conf import re_path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('',views.home,name='home'),
     path('aboutus',views.aboutus,name='aboutus'),
     path('contactus',views.contactus,name='contactus'),
     path('delete/<int:id>/',views.delete_data,name='deletedata'),
-    path('<int:id>/',views.update_data,name='updatedata')
+    path('<int:id>/',views.update_data,name='updatedata'),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
-if settings.DEBUG:
-    urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+ #   urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
