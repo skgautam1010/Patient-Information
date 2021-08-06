88da0dcdc71476ee23c50df2361c14dc27d5affc
Sonu Kumar Gautam <sonukumargautam41638@gmail.com>
2021-08-06 14:46:30 +0530

    final commit

--- patientrecord/settings.py
+++ patientrecord/settings.py
@@ -126,7 +126,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 STATIC_URL = '/static/'
-STATIC_ROOT=BASE_DIR / 'static'
+STATIC_ROOT= BASE_DIR / 'static'
 APPEND_SLASH=False
 
 
