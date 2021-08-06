4b37669ccb64617789f7348cefa6873ff941ba07
Sonu Kumar Gautam <sonukumargautam41638@gmail.com>
2021-08-06 15:36:11 +0530

    final commit

--- patientrecord/settings.py
+++ patientrecord/settings.py
@@ -125,12 +125,13 @@ APPEND_SLASH=False
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT= os.path.join(BASE_DIR, 'static')
-
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+#STATIC_ROOT= os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'static')
+]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = 'media'
 
 
 
